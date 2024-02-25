ce99d5c3af8e961954effc26169b97013f484a50
steffenskov <steffen@ckode.dk>
2024-02-25 15:37:26 +0100

    chore: Updated github action in CD

--- tests/SegregatedStorage.UnitTests/Services/KeyServiceLocatorTests.cs
+++ tests/SegregatedStorage.UnitTests/Services/KeyServiceLocatorTests.cs
@@ -24,7 +24,7 @@ public class KeyServiceLocatorTests
 	public void GetService_Exists_ReturnsSameInstance()
 	{
 		// Arrange
-		var locator = new KeyServiceLocator<int, Service>(val => new Service(val));
+		var locator = new KeyServiceLocator<int, Service>(val => new Service());
 
 		// Act
 		var firstService = locator.GetService(42);
@@ -38,7 +38,7 @@ public class KeyServiceLocatorTests
 	public void GetServices_NoneCreated_ReturnsEmptyCollection()
 	{
 		// Arrange
-		var locator = new KeyServiceLocator<int, Service>(val => new Service(val));
+		var locator = new KeyServiceLocator<int, Service>(val => new Service());
 
 		// Act
 		var services = locator.GetServices();
@@ -51,7 +51,7 @@ public class KeyServiceLocatorTests
 	public void GetServices_SomeCreated_ReturnsThose()
 	{
 		// Arrange
-		var locator = new KeyServiceLocator<int, Service>(val => new Service(val));
+		var locator = new KeyServiceLocator<int, Service>(val => new Service());
 		var service1 = locator.GetService(42);
 		var service2 = locator.GetService(1337);
 
@@ -64,4 +64,4 @@ public class KeyServiceLocatorTests
 	}
 }
 
-file class Service(int Value);
+file class Service();
